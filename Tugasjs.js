f725ff1e8178fea4f3db0c2f87b37b026e419e11
Firgiealfz <firgie2020@gmail.com>
2025-10-06 07:23:13 +0700

    Tugas firgie

---
+++ Tugasjs.js
@@ -0,0 +1,18 @@
+const a = 30
+const b = 20
+
+
+const tambah = () => a + b;
+console.log(tambah(a,b))
+
+const kurang = () => a - b;
+console.log(kurang(a,b))
+
+const kali = () => a * b;
+console.log(kali(a,b))
+
+const bagi = () => a / b;
+console.log(bagi(a,b))
+
+const hasilbagi = () => a % b;
+console.log(hasilbagi(a,b))
